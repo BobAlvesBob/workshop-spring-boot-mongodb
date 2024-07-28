874dc696b5193be62659224d38c5c02bde13ce35
Bob <bobpitaia@gmail.com>
2024-07-28 15:11:28 -0300

    Obtendo post por id

---
+++ src/main/java/com/alvesbob/workshopmongo/resources/PostResource.java
@@ -0,0 +1,32 @@
+package com.alvesbob.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alvesbob.workshopmongo.domain.Post;
+import com.alvesbob.workshopmongo.dto.UserDTO;
+import com.alvesbob.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService service;
+	
+	
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Post> findById(@PathVariable("id") String id){
+	
+	Post obj = service.findById(id);
+	
+	return ResponseEntity.ok().body(obj);
+	}
+		
+
+}
